2194e607bc89f26a61cc206405be5c7929073ed9
Ryan <jrcanty@gmail.com>
2013-02-11 14:03:45 -0800

    Initial Commit

---
+++ FiveFish/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FiveFish
+//
+//  Created by Ryan Canty on 2/11/13.
+//  Copyright (c) 2013 GRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
